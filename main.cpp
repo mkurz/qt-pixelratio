8cf4bc8a2eaee2fb0b1fa0b9d261809a6511d5b5
Matthias Kurz <m.kurz@irregular.at>
2025-11-07 09:01:34 +0100

    output details about all screens

--- main.cpp
+++ main.cpp
@@ -4,27 +4,26 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QScreen *screen = QApplication::primaryScreen();
+    const QList<QScreen *> screens = QApplication::screens();
 
-    if (!screen) {
-        std::cerr << "No primary screen found." << std::endl;
+    if (screens.isEmpty()) {
+        std::cerr << "No screens detected." << std::endl;
         return 1;
     }
 
-    double dpr = screen->devicePixelRatio();
-    double logicalDpiX = screen->logicalDotsPerInchX();
-    double logicalDpiY = screen->logicalDotsPerInchY();
-    double physicalDpiX = screen->physicalDotsPerInchX();
-    double physicalDpiY = screen->physicalDotsPerInchY();
-
-    std::cout << "----------------------------------------------------------------" << std::endl;
-    std::cout << "Primary screen: " << screen->name().toStdString() << std::endl;
-    std::cout << "----------------------------------------------------------------" << std::endl;
-    std::cout << "Device pixel ratio:                           " << dpr << std::endl;
-    std::cout << "Logical DPI:                                  " << logicalDpiX << " x " << logicalDpiY << std::endl;
-    std::cout << "Physical DPI:                                 " << physicalDpiX << " x " << physicalDpiY << std::endl;
-
-    std::cout << "logicalDotsPerInchX / physicalDotsPerInchX:   " << ((qreal)logicalDpiX / (qreal)physicalDpiX) << std::endl;
+    for (int i = 0; i < screens.size(); ++i) {
+        QScreen *screen = screens[i];
+        std::cout << "----------------------------------------------------------------" << std::endl;
+        std::cout << "Screen " << i + 1 << ": " << screen->name().toStdString() << std::endl;
+        std::cout << "----------------------------------------------------------------" << std::endl;
+        std::cout << "Device pixel ratio:                           " << screen->devicePixelRatio() << std::endl;
+        std::cout << "Logical DPI:                                  " << screen->logicalDotsPerInchX()
+                  << " x " << screen->logicalDotsPerInchY() << std::endl;
+        std::cout << "Physical DPI:                                 " << screen->physicalDotsPerInchX()
+                  << " x " << screen->physicalDotsPerInchY() << std::endl;
+        std::cout << "logicalDotsPerInchX / physicalDotsPerInchX:   " << ((qreal)screen->logicalDotsPerInchX() / (qreal)screen->physicalDotsPerInchX()) << std::endl;
+        std::cout << std::endl;
+    }
 
     return 0;
 }
